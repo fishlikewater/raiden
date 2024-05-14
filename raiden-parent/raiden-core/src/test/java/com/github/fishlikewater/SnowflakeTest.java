a69ee2ab7211e3741694d40046b68ca304d8b8d8
zhangxiang <zhangxiang@uphicoo.com>
2024-05-14 12:14:18 +0800

    feat: update head

--- raiden-parent/raiden-core/src/test/java/com/github/fishlikewater/SnowflakeTest.java
+++ raiden-parent/raiden-core/src/test/java/com/github/fishlikewater/SnowflakeTest.java
@@ -1,6 +1,4 @@
 /*
- * Copyright © 2024 zhangxiang (fishlikewater@126.com)
- *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
