68fecc3d4838f21aaac33df0ac8ac3ace6692375
zhangxiang <zhangxiang@uphicoo.com>
2024-05-15 17:31:13 +0800

    feat: add lock core

---
+++ raiden-parent/raiden-lock/lock-core/src/main/java/com/github/fishlikewater/raiden/lock/core/RedisLock.java
@@ -0,0 +1,53 @@
+/*
+ * Copyright (c) 2024 zhangxiang (fishlikewater@126.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.github.fishlikewater.raiden.lock.core;
+
+import org.redisson.api.RLock;
+import org.redisson.api.RedissonClient;
+
+/**
+ * {@code RedisLock}
+ * Redis 锁
+ *
+ * @author zhangxiang
+ * @version 1.0.0
+ * @since 2024/05/15
+ */
+public interface RedisLock {
+
+    /**
+     * 获取redis客户端
+     *
+     * @return redis客户端
+     */
+    RedissonClient redisClient();
+
+    /**
+     * 尝试获取锁
+     *
+     * @param key      锁的key
+     * @param runnable 获取到锁后的执行逻辑
+     */
+    default void tryLock(String key, Runnable runnable) {
+        RLock lock = this.redisClient().getLock(key);
+        lock.lock();
+        try {
+            runnable.run();
+        } finally {
+            lock.unlock();
+        }
+    }
+}
