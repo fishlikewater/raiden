a1ae8df5b22fa6dda32ac5b5294513696caa5d59
zhangxiang <zhangxiang@uphicoo.com>
2024-07-23 14:35:53 +0800

    refactor(config): use ConfigConstants for default config name

---
+++ raiden-parent/raiden-config/src/main/java/io/github/fishlikewater/raiden/config/constants/ConfigConstants.java
@@ -0,0 +1,15 @@
+package io.github.fishlikewater.raiden.config.constants;
+
+/**
+ * {@code ConfigConstants}
+ *
+ * @author zhangxiang
+ * @version 1.0.5
+ * @since 2024/07/23
+ */
+public interface ConfigConstants {
+
+    String DEFAULT_CONFIG_NAME = "default";
+
+
+}
