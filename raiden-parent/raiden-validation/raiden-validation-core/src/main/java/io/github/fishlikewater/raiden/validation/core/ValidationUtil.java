03094b6ff7ff6f5e160e0fdec25362552b6db1f5
fish <fishlikewater@126.com>
2024-09-28 22:42:21 +0800

    feat: 添加静态工具类

--- raiden-parent/raiden-validation/raiden-validation-core/src/main/java/io/github/fishlikewater/raiden/validation/core/ValidationUtil.java
+++ raiden-parent/raiden-validation/raiden-validation-core/src/main/java/io/github/fishlikewater/raiden/validation/core/ValidationUtil.java
@@ -1,9 +1,11 @@
 package io.github.fishlikewater.raiden.validation.core;
 
+import io.github.fishlikewater.raiden.core.CollectionUtils;
 import io.github.fishlikewater.raiden.core.ObjectUtils;
-import jakarta.validation.Validation;
-import jakarta.validation.Validator;
-import jakarta.validation.ValidatorFactory;
+import jakarta.validation.*;
+
+import java.util.Collection;
+import java.util.Set;
 
 /**
  * ValidationUtil
@@ -26,5 +28,19 @@ public class ValidationUtil {
             validator = factory.getValidator();
         }
         return validator;
+    }
+
+    public <T> void validate(Collection<T> ts, Class<?>... groups) {
+        ts.forEach((t) -> {
+            Set<ConstraintViolation<T>> violations;
+            if (ObjectUtils.isNullOrEmpty(groups)) {
+                violations = getValidator().validate(t);
+            } else {
+                violations = getValidator().validate(t, groups);
+            }
+            if (!CollectionUtils.isEmpty(violations)) {
+                throw new ConstraintViolationException(violations);
+            }
+        });
     }
 }
