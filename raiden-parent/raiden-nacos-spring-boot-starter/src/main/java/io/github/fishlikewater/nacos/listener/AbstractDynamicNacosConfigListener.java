0281d25ec9710640f5df98aee0137ec687889c0c
zhangxiang <fishlikewater@126.com>
2024-10-29 16:55:49 +0800

    feat: nacos

--- raiden-parent/raiden-nacos-spring-boot-starter/src/main/java/io/github/fishlikewater/nacos/listener/AbstractDynamicNacosConfigListener.java
+++ raiden-parent/raiden-nacos-spring-boot-starter/src/main/java/io/github/fishlikewater/nacos/listener/AbstractDynamicNacosConfigListener.java
@@ -1,13 +1,17 @@
 /*
- * Copyright (c) 2023 Uphicoo Co., Ltd. All rights reserved.
+ * Copyright (c) 2024 zhangxiang (fishlikewater@126.com)
  *
- * This software is owned by Uphicoo Co., Ltd.
- * Without the official authorization of Uphicoo Co., Ltd.,
- * no enterprise or individual can obtain, read, install,
- * or disseminate any content protected by intellectual
- * property rights involved in this software.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- * The website of uphicoo, please see <https://uphicoo.com>
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package io.github.fishlikewater.nacos.listener;
 
