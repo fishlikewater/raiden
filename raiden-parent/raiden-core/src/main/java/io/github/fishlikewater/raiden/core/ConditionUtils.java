e4b0bd880f24437c7c9d2279368ab255ce53cef7
zhangxiang <fishlikewater@126.com>
2025-01-20 10:33:03 +0800

    feat(core): 添加条件工具类 ConditionUtils
    - 新增 ConditionUtils 类，提供条件处理相关方法
    - 实现 Condition 内部类，用于封装条件结果和执行状态- 添加多个条件判断方法，支持链式调用
    - 编写 ConditionUtilsTest 测试类，验证功能正确性

---
+++ raiden-parent/raiden-core/src/main/java/io/github/fishlikewater/raiden/core/ConditionUtils.java
@@ -0,0 +1,102 @@
+/*
+ * Copyright (c) 2025 zhangxiang (fishlikewater@126.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.github.fishlikewater.raiden.core;
+
+import lombok.Data;
+
+import java.util.function.Supplier;
+
+/**
+ * {@code ConditionUtils}
+ * 条件处理工具
+ *
+ * @author zhangxiang
+ * @version 1.1.3
+ * @since 2025/01/20
+ */
+public class ConditionUtils {
+
+    public static Condition of(Supplier<Boolean> supplier) {
+        return new Condition(supplier.get(), Status.INIT);
+    }
+
+    @Data
+    public static class Condition {
+
+        private boolean result;
+
+        private Status status;
+
+        private Condition(boolean result, Status status) {
+            this.result = result;
+            this.status = status;
+        }
+
+        public void isTrue(Runnable runnable) {
+            if (this.result && (this.status == Status.INIT || this.status == Status.IS_EXECUTE)) {
+                runnable.run();
+            }
+        }
+
+        public Condition isTrue(Supplier<Boolean> supplier) {
+            if (this.result && (this.status == Status.INIT || this.status == Status.IS_EXECUTE)) {
+                this.status = Status.IS_EXECUTE;
+                this.result = supplier.get();
+            } else if (this.status != Status.INIT) {
+                this.status = Status.NO_EXECUTE;
+            }
+
+            return this;
+        }
+
+        public Condition orElse(Supplier<Boolean> supplier) {
+            if (this.status == Status.NO_EXECUTE || this.status == Status.INIT) {
+                this.result = supplier.get();
+                this.status = Status.IS_EXECUTE;
+            }
+
+            return this;
+        }
+
+        public Condition or(Supplier<Boolean> supplier) {
+            this.result = this.result || supplier.get();
+
+            return this;
+        }
+
+        public Condition and(Supplier<Boolean> supplier) {
+            this.result = this.result && supplier.get();
+
+            return this;
+        }
+    }
+
+    protected enum Status {
+
+        /**
+         * 初始状态
+         */
+        INIT,
+        /**
+         * 上一步执行状态
+         */
+        IS_EXECUTE,
+        /**
+         * 上一步未执行状态
+         */
+        NO_EXECUTE,
+    }
+}
---
+++ raiden-parent/raiden-core/src/test/java/io/github/fishlikewater/ConditionUtilsTest.java
@@ -0,0 +1,71 @@
+/*
+ * Copyright (c) 2025 zhangxiang (fishlikewater@126.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.github.fishlikewater;
+
+import io.github.fishlikewater.raiden.core.ConditionUtils;
+import org.junit.Test;
+
+/**
+ * {@code ConditionUtilsTest}
+ * 条件工具测试
+ *
+ * @author zhangxiang
+ * @version 1.1.3
+ * @since 2025/01/20
+ */
+public class ConditionUtilsTest {
+
+    @Test
+    public void testConditionUtils() {
+        ConditionUtils.of(() -> true)
+                .isTrue(() -> false)
+                .orElse(() -> true)
+                .and(() -> true)
+                .or(() -> true)
+                .isTrue(() -> true)
+                .isTrue(() -> System.out.println("11111"));
+    }
+
+    @Test
+    @SuppressWarnings("all")
+    public void testConditionUtils2() {
+        String name = "fishlikewater";
+        ConditionUtils.of(() -> name.startsWith("fish"))
+                .isTrue(() -> {
+                    System.out.println("isTrue");
+                    System.out.println("fish");
+                    return true;
+                })
+                .orElse(() -> {
+                    System.out.println("orElse");
+                    System.out.println("not fish");
+                    return false;
+                })
+                .and(() -> {
+                    System.out.println("and");
+                    return name.length() > 10;
+                })
+                .or(() -> {
+                    System.out.println("or");
+                    return name.endsWith("water");
+                })
+                .isTrue(() -> {
+                    System.out.println("isTrue");
+                    return true;
+                })
+                .isTrue(() -> System.out.println("11111"));
+    }
+}
