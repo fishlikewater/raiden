51032b7d64502ae8b2a0152c542b047a6be8c5cd
zhangxiang <fishlikewater@126.com>
2025-02-28 11:07:43 +0800

    feat(json-core): 添加 Jackson 工具类方法
    - 新增 toJsonString 方法，用于将对象转换为 JSON 字符串
    - 新增多个 parseObject 方法，支持将 JSON 字符串或字节数组转换为指定类型的对象
    - 新增 parseArray 方法，用于将 JSON 字符串转换为指定类型对象的列表
    - 优化了异常处理，使用 RuntimeException 包装原生异常
    - 引入了 CollectionUtils、ObjectUtils、StringUtils

--- raiden-parent/raiden-json/json-core/src/main/java/io/github/fishlikewater/raiden/json/core/jackson/JacksonUtils.java
+++ raiden-parent/raiden-json/json-core/src/main/java/io/github/fishlikewater/raiden/json/core/jackson/JacksonUtils.java
@@ -15,7 +15,16 @@
  */
 package io.github.fishlikewater.raiden.json.core.jackson;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import io.github.fishlikewater.raiden.core.CollectionUtils;
+import io.github.fishlikewater.raiden.core.ObjectUtils;
+import io.github.fishlikewater.raiden.core.StringUtils;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * {@code JacksonUtils}
@@ -30,4 +39,101 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 @SuppressWarnings("all")
 public class JacksonUtils extends ObjectMapper {
 
+    /**
+     * 将对象转换为JSON格式的字符串
+     *
+     * @param object 要转换的对象
+     * @return JSON格式的字符串，如果对象为null，则返回null
+     * @throws RuntimeException 如果转换过程中发生JSON处理异常，则抛出运行时异常
+     */
+    public String toJsonString(Object object) {
+        if (ObjectUtils.isNullOrEmpty(object)) {
+            return null;
+        }
+        try {
+            return this.writeValueAsString(object);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    /**
+     * 将JSON格式的字符串转换为指定类型的对象
+     *
+     * @param text  JSON格式的字符串
+     * @param clazz 要转换的目标对象类型
+     * @param <T>   目标对象的泛型类型
+     * @return 转换后的对象，如果字符串为空则返回null
+     * @throws RuntimeException 如果转换过程中发生IO异常，则抛出运行时异常
+     */
+    public <T> T parseObject(String text, Class<T> clazz) {
+        if (StringUtils.isBlank(text)) {
+            return null;
+        }
+        try {
+            return this.readValue(text, clazz);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    /**
+     * 将字节数组转换为指定类型的对象
+     *
+     * @param bytes 字节数组
+     * @param clazz 要转换的目标对象类型
+     * @param <T>   目标对象的泛型类型
+     * @return 转换后的对象，如果字节数组为空则返回null
+     * @throws RuntimeException 如果转换过程中发生IO异常，则抛出运行时异常
+     */
+    public <T> T parseObject(byte[] bytes, Class<T> clazz) {
+        if (CollectionUtils.isEmpty(bytes)) {
+            return null;
+        }
+        try {
+            return this.readValue(bytes, clazz);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    /**
+     * 将JSON格式的字符串转换为指定类型的对象，支持复杂类型
+     *
+     * @param text          JSON格式的字符串
+     * @param typeReference 指定类型的TypeReference对象
+     * @param <T>           目标对象的泛型类型
+     * @return 转换后的对象，如果字符串为空则返回null
+     * @throws RuntimeException 如果转换过程中发生IO异常，则抛出运行时异常
+     */
+    public <T> T parseObject(String text, TypeReference<T> typeReference) {
+        if (StringUtils.isBlank(text)) {
+            return null;
+        }
+        try {
+            return this.readValue(text, typeReference);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    /**
+     * 将JSON格式的字符串转换为指定类型对象的列表
+     *
+     * @param text  JSON格式的字符串
+     * @param clazz 要转换的目标对象类型
+     * @param <T>   目标对象的泛型类型
+     * @return 转换后的对象的列表，如果字符串为空则返回空列表
+     * @throws RuntimeException 如果转换过程中发生IO异常，则抛出运行时异常
+     */
+    public <T> List<T> parseArray(String text, Class<T> clazz) {
+        if (StringUtils.isBlank(text)) {
+            return new ArrayList<>();
+        }
+        try {
+            return this.readValue(text, this.getTypeFactory().constructCollectionType(List.class, clazz));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
